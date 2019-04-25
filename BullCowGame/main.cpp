58958993b02ba4489244e37f75291e11f2633b6c
justind85 <falsemotive@icloud.com>
2019-04-24 19:54:51 -0600

    Add Intro function

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-
+void PrintIntro() {
 	constexpr int WORD_LENGTH = 7;
 	cout << "Welcome to Bulls and Cows." << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+}
+int main() {
+	// Game introduction
+	PrintIntro();
+
+	// Get guess from player
+	string Guess = "";
+	getline(cin, Guess);
+	cout << "your guess was " << Guess << endl;
+	// repeat the guess back to player
 	return 0;
 }
