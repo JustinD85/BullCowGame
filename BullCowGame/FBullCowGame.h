db85a1b3def3e3c9181daa412506c1e5f2a7d77a
justind85 <falsemotive@icloud.com>
2019-04-25 09:12:19 -0600

    Add BullCowGame class

---
+++ BullCowGame/FBullCowGame.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class FBullCowGame {
+public:
+    void Reset(); //TODO enhance return value.
+    int GetMaxTries();
+    int GetCurrentTry;
+    bool IsGameWon();
+    bool CheckGuessValidity(std::string); //TODO enhance return value.
+
+
+
+
+private:
+    int MyCurrentTry;
+    int MyMaxTries;
+    bool IsIsoGram();
+};
