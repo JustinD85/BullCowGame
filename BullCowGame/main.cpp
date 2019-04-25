47bf5b96a6b6b1e50e0ab44e716b7758594cc29a
justind85 <falsemotive@icloud.com>
2019-04-24 20:59:27 -0600

    Refactor main for readability

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,19 +3,29 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
+
+//Entry point of application
+int main() {
+	
+	PrintIntro();
+	GetGuess();
+	return 0;
+}
+
+// Game introduction
 void PrintIntro() {
 	constexpr int WORD_LENGTH = 7;
 	cout << "Welcome to Bulls and Cows." << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 }
-int main() {
-	// Game introduction
-	PrintIntro();
 
-	// Get guess from player
+// Get guess from player
+string GetGuess() {
 	string Guess = "";
 	getline(cin, Guess);
-	cout << "your guess was " << Guess << endl;
 	// repeat the guess back to player
-	return 0;
+	cout << "your guess was " << Guess << endl;
+	return Guess;
 }
