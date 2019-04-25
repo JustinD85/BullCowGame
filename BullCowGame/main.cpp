a1c1c08577b25b982895a93ef0c58734989e1a76
justind85 <falsemotive@icloud.com>
2019-04-24 21:21:04 -0600

    Change to spaces instead of tabs

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -8,29 +8,29 @@ string GetGuess();
 
 //Entry point of application
 int main() {
-	
-	PrintIntro();
 
-	// loop for number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int i = 0; i < NUMBER_OF_TURNS; i++) {
-		GetGuess();
-	}
-	return 0;
+    PrintIntro();
+
+    // loop for number of turns asking for guesses
+    constexpr int NUMBER_OF_TURNS = 5;
+    for (int i = 0; i < NUMBER_OF_TURNS; i++) {
+        GetGuess();
+    }
+    return 0;
 }
 
 // Game introduction
 void PrintIntro() {
-	constexpr int WORD_LENGTH = 7;
-	cout << "Welcome to Bulls and Cows." << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+    constexpr int WORD_LENGTH = 7;
+    cout << "Welcome to Bulls and Cows." << endl;
+    cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 }
 
 string GetGuess() {
-	// Get guess from player
-	string Guess = "";
-	getline(cin, Guess);
-	// repeat the guess back to player
-	cout << "your guess was " << Guess << endl;
-	return Guess;
+    // Get guess from player
+    string Guess = "";
+    getline(cin, Guess);
+    // repeat the guess back to player
+    cout << "your guess was " << Guess << endl;
+    return Guess;
 }
